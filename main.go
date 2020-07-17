0cb2d013cd0283dc6b1223e41de70314f47caca1
justincaseof <justin.caseof@gmail.com>
2020-07-17 20:37:40 +0200

    stuff

--- main.go
+++ main.go
@@ -1,54 +1,152 @@
 package main
 
 import (
-    "bytes"
-    "log"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
-    "net/http"
+
 	"github.com/gorilla/mux"
 )
 
+const ConfigFileName = "config.json"
+
+type Preset struct {
+	Index   uint8
+	Name    string
+	IconURL string
+}
+
+type Config struct {
+	YamahaReceiverHost string `json:"yamahaReceiverHost"` // IP or FQDN
+	Presets            []Preset
+	Server             struct {
+		ListenPort uint16
+	}
+}
+
+var shutdownSig chan os.Signal
+var config Config
+
 func main() {
-	presetHandler := func(w http.ResponseWriter, r *http.Request) {
-		log.Println("###")
-		
-		vars := mux.Vars(r)
-		presetNum := vars["presetNum"]
-		
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		if r.Method == http.MethodOptions {
-			return
-		}
-		
-		var lala = bytes.Buffer{}
-		lala.WriteString("<YAMAHA_AV cmd=\"PUT\"><NET_RADIO><Play_Control><Preset><Preset_Sel>" + presetNum + "</Preset_Sel></Preset></Play_Control></NET_RADIO></YAMAHA_AV>")
-		
-		_, err := http.Post("http://192.168.178.7/YamahaRemoteControl/ctrl", "text/xml", &lala)
-		if err != nil {
-			log.Println("FAIL")
-		} else {
-			log.Println("OK")
-		}
-		
-		
-		w.Write([]byte("OK"))
+	LoadConfiguration(&config)
+	defer log.Println("Exit.")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/preset/{presetNum}", presetHandler).Methods(http.MethodGet)
+	r.HandleFunc("/exit", exitHandler).Methods(http.MethodGet)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./")))
+
+	r.Use(mux.CORSMethodMiddleware(r))
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:9000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
 	}
 
-    r := mux.NewRouter()
+	log.Println("Starting Server using Yamaha RX-V771 @ " + config.YamahaReceiverHost)
+	go srv.ListenAndServe()
 
-    // This will serve files under http://localhost:9000/static/<filename>
-    r.HandleFunc("/preset/{presetNum}", presetHandler).Methods(http.MethodGet)
-	r.PathPrefix("/").Handler( http.FileServer( http.Dir("./") ))
-	
-	r.Use(mux.CORSMethodMiddleware(r))
-    
-	srv := &http.Server{
-        Handler:      r,
-        Addr:         "127.0.0.1:9000",
-        // Good practice: enforce timeouts for servers you create!
-        WriteTimeout: 15 * time.Second,
-        ReadTimeout:  15 * time.Second,
-    }
+	shutdownSig = make(chan os.Signal)
+	signal.Notify(shutdownSig, os.Interrupt)
+	signal.Notify(shutdownSig, syscall.SIGKILL)
 
-    log.Fatal(srv.ListenAndServe())
+	// wait for our death
+	<-shutdownSig
+	// and persist our state
+	PersistConfiguration(&config)
+
+}
+
+// =============================================================================================
+
+func LoadConfiguration(config *Config) {
+	configFile, err := os.Open(ConfigFileName)
+	defer configFile.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(config)
+}
+
+func PersistConfiguration(config *Config) {
+	configFile, err := os.OpenFile(ConfigFileName, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer configFile.Close()
+
+	err = os.Truncate(ConfigFileName, 0)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	_buf, err := json.MarshalIndent(config, "", "    ")
+	if err == nil {
+		_, err = configFile.Write(_buf)
+		if err == nil {
+			log.Println("Config written.")
+			return
+		}
+	}
+	log.Fatal("Couldn't write config:")
+	log.Fatal(err)
+}
+
+func isFile(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// =============================================================================================
+
+func exitHandler(w http.ResponseWriter, r *http.Request) {
+	//defer signal.Notify(shutdownSig, os.Interrupt)	// doesnt work
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	log.Println("Exit requested")
+	w.Write([]byte("OK"))
+
+	shutdownSig <- os.Interrupt
+}
+
+func presetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	presetNum := vars["presetNum"]
+	log.Println("### Preset: " + presetNum)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	var lala = bytes.Buffer{}
+	lala.WriteString("<YAMAHA_AV cmd=\"PUT\"><NET_RADIO><Play_Control><Preset><Preset_Sel>" + presetNum + "</Preset_Sel></Preset></Play_Control></NET_RADIO></YAMAHA_AV>")
+
+	_, err := http.Post("http://"+config.YamahaReceiverHost+"/YamahaRemoteControl/ctrl", "text/xml", &lala)
+	if err != nil {
+		log.Println("FAIL")
+	} else {
+		log.Println("OK")
+	}
+
+	w.Write([]byte("OK"))
 }
--- post.js
+++ post.js
@@ -16,14 +16,21 @@ function send (preset){
          console.warn(request.statusText, request.responseText);
       }
    });
-   /*request.open("POST","http://192.168.178.7/YamahaRemoteControl/ctrl");		// CORS :-(
-   request.setRequestHeader("Content-Length",""+data.length);
-   request.setRequestHeader("Content-Type","text/xml");
-   request.setRequestHeader("Origin", 'FOO');
-   request.send(data);
-   */
    console.log("## >>>> Preset: " + preset);
    request.open("GET","http://127.0.0.1:9000/preset/" + preset);
    request.send();
    console.log("## <<<<");
+}
+
+function shutdown (){
+   var request = new XMLHttpRequest();
+   request.addEventListener('load', function(event) {
+      if (request.status >= 200 && request.status < 300) {
+         console.log(request.responseText);
+      } else {
+         console.warn(request.statusText, request.responseText);
+      }
+   });
+   request.open("GET","http://127.0.0.1:9000/exit");
+   request.send();
 }
